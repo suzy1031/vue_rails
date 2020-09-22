76dcc28a00ec060ab2281f88f955debd80c59ab5
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-09-22 10:23:26 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  # APIコントローラへのルーティング
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :employees, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+end
