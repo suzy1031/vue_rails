62c36b2ab73914c1edd5cb71e307ceeada501c32
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-09-29 07:45:40 +0900

    add gem file

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,13 @@ module VueRails
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
   end
 end
